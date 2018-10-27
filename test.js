4c5a27be4b1b358cfa0f1471b54f4dd8c4d0b399
IngvarOldd <lyushinskij.i.a@gmail.com>
2018-10-28 01:20:24 +0300

    refactor: add 2 more test for the 1st task

--- test.js
+++ test.js
@@ -7,4 +7,12 @@ describe('sumOfOther test', () => {
         const sum = sumOfOther([2, 3, 4, 1]);
         assert.deepEqual(sum, [8, 7, 6, 9]);
     });
+    it('1', () => {
+        const sum = sumOfOther([5, 10, 8, 21, 6]);
+        assert.deepEqual(sum, [45, 40, 42, 29, 44]);
+    });
+    it ('2', () => {
+        const sum = sumOfOther([9, 8, 7, 6, 5, 4, 3, 2, 1]);
+        assert.deepEqual(sum, [36, 37, 38, 39, 40, 41, 42, 43, 44]);
+    });
 });
