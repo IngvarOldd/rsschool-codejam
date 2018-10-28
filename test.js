74c009f6aaf11905fa0ad05bdff3bda7a5a46a74
IngvarOldd <lyushinskij.i.a@gmail.com>
2018-10-28 14:24:07 +0300

    feat: add 2nd task and test for it

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 Object.freeze(assert);
 const sumOfOther = require('./sumOfOther/sumOfOther.js');
+const make = require('./make/make.js');
 
 describe('sumOfOther test', () => {
     it('0', () => {
@@ -14,5 +15,23 @@ describe('sumOfOther test', () => {
     it ('2', () => {
         const sum = sumOfOther([9, 8, 7, 6, 5, 4, 3, 2, 1]);
         assert.deepEqual(sum, [36, 37, 38, 39, 40, 41, 42, 43, 44]);
+    });
+});
+
+describe('make test', () => {
+    it('0', () => {
+        function sum(a, b) {return a+b}; 
+        const makeFunc = make(15)(34, 21, 666)(41)(sum);
+        assert.deepEqual(makeFunc, 777);
+    });
+    it ('1', () => {
+        function res(a, b) {return a-b};
+        const makeFunc = make(100)(12, 2, 49)(3)(res);
+        assert.deepEqual(makeFunc, 34);
+    });
+    it ('2', () => {
+        function multipl(a, b) {return a*b};
+        const makeFunc = make(2)(1, 4, 5)(10)(multipl);
+        assert.deepEqual(makeFunc, 400);
     });
 });
